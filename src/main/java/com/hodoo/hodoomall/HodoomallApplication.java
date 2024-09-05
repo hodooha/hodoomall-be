f59b92ebe6e5586cd427facbda68f545f0de1f64
JieunHa <148246588+hodooha@users.noreply.github.com>
2024-09-05 14:59:18 +0900

    Initial commit

---
+++ src/main/java/com/hodoo/hodoomall/HodoomallApplication.java
@@ -0,0 +1,13 @@
+package com.hodoo.hodoomall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HodoomallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HodoomallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hodoo/hodoomall/HodoomallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hodoo.hodoomall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HodoomallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
